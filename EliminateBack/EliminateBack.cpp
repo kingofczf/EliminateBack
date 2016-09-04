9d02a304157f5d098fa1f5da964231312f9b3be3
kingofczf <kingofczf@sina.com>
2016-09-04 22:13:51 +0800

    完成子函数的设计，暂未调试

--- EliminateBack/EliminateBack.cpp
+++ EliminateBack/EliminateBack.cpp
@@ -1,11 +1,48 @@
-// EliminateBack.cpp : 定义控制台应用程序的入口点。
-//
+#include<iostream>
+#include<string>
+using namespace std;
 
-#include "stdafx.h"
-
-
-int main()
+/**********************寻找最短字符串的位置**********************************************/
+int findMin(string wenfa[], int n)//n为文法数量,wenfa[0]存放匹配的字符
 {
-    return 0;
+	int location;
+	int minLength = wenfa[1].length;
+	for (int i = 1; i < n; i++)
+	{
+		
+		if(minLength>=wenfa[i].length)
+		{
+			minLength = wenfa[i].length;
+			location = i;
+		}
+	}
+		return location;
+}
+/**********************复制最短字符串，拆解***************************************/
+void spiltWenfa(string wenfa[],int i,int location)//i为需要拆分的位置
+{	
+	wenfa[0] = wenfa[location].at[i];
 }
 
+
+/***********************寻找匹配字符串*******************************************/
+
+bool match(string wenfa[],int n,int location)
+{
+	int count = 0;
+	for (int no = 1; no < n; no++)
+	{
+		for (int num = 0; num < wenfa[no].length; num++)
+		{
+			if (wenfa[0] == wenfa[no].at[num])
+				count++;
+		}
+	}
+	if (count == n) return true;
+	else false;
+}
+
+void main()
+{
+
+}
